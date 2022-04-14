53905d779a979e2d6f924c0f1e0e18f4a01b6341
moth <no>
2022-04-14 18:35:04 -0500

    made IMDb scraper does not work as intended and switching to tmdb [trans rights]

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,8 @@ pub mod stringinitials;
 pub mod anime;
 
 fn main() {
-    println!("{:?}", verse::getvinitials());
+    //println!("{:?}", verse::getvinitials());
+    let Anime = anime::init().unwrap();
+    //println!("{:?}", anime::get(Anime));
+    println!("{:?}", anime::get(Anime));
 }
